b89d069136877c1018386b185dbe340daf6b3535
lerenardprudent <daniel.margulius@gmail.com>
2018-10-11 13:39:17 -0400

    Go.

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -1,25 +1,146 @@
-<?php
-
-$db = DBManagerFactory::getInstance();
-$cond = "1";
-$codeCol = "code";
-$descCol = "desc_en";
-$tableName = "cim9";
-$query = "select $codeCol, $descCol from $tableName where $cond order by $descCol";
-$result= $db->query($query);
-
-$GLOBALS['app_list_strings']['cim9_list']=array ();
-while ( $row= $db->fetchRow($result) ) {
-  $GLOBALS['app_list_strings']['cim9_list'][$row[$codeCol]] = $row[$descCol];
-}
-/*$GLOBALS['app_list_strings']['cim9_list']=array (
-  '050' => 'Smallpox',
-  '055.9' => 'Measles',
-);*/
-
-$GLOBALS['app_list_strings']['yes_no_list']=array (
-  'oui' => 'Yes',
-  'non' => 'No',
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+$db = DBManagerFactory::getInstance();
+
+
+
+
+
+
+$cond = "1";
+
+
+
+
+
+
+$codeCol = "code";
+
+
+
+
+
+
+$descCol = "desc_en";
+
+
+
+
+
+
+$tableName = "cim9";
+
+
+
+
+
+
+$query = "select $codeCol, $descCol from $tableName where $cond order by $descCol";
+
+
+
+
+
+
+$result= $db->query($query);
+
+
+
+
+
+
+
+
+
+
+
+
+
+$GLOBALS['app_list_strings']['cim9_list']=array ();
+
+
+
+
+
+
+while ( $row= $db->fetchRow($result) ) {
+
+
+
+
+
+
+  $GLOBALS['app_list_strings']['cim9_list'][$row[$codeCol]] = $row[$descCol];
+
+
+
+
+
+
+}
+
+
+
+
+
+
+/*$GLOBALS['app_list_strings']['cim9_list']=array (
+
+
+
+
+
+
+  '050' => 'Smallpox',
+
+
+
+
+
+
+  '055.9' => 'Measles',
+
+
+
+
+
+
+);*/
+
+
+
+
+
+
+
+
+
+$GLOBALS['app_list_strings']['yes_no_list']=array (
+
+
+
+
+  'oui' => 'Yes',
+
+
+
+
+  'non' => 'No',
+
+
+
+
 );
 
 $GLOBALS['app_list_strings']['etab_sante_list']=array (
