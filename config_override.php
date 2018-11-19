6fce28edf00c3d6fe9291971b7d690f7ab31f2c6
lerenardprudent <daniel.margulius@gmail.com>
2018-11-19 12:53:18 -0500

    DB

--- config_override.php
+++ config_override.php
@@ -9,4 +9,5 @@ $sugar_config['default_module_favicon'] = false;
 $sugar_config['dashlet_auto_refresh_min'] = '30';
 $sugar_config['stack_trace_errors'] = false;
 $sugar_config['developerMode'] = false;
+$sugar_config['http_referer']['list'][] = 'ceppp.ca';
 /***CONFIGURATOR***/
